8611a77e863ed4fabc6f21a92a55bd9f7aa34a6f
RomanLesovoy <romadesire97@gmail.com>
2024-10-27 13:49:32 +0200

    feat: optimized blocks load and render

--- src/app/block-map/block-map.component.ts
+++ src/app/block-map/block-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { BlockService } from '../services/block.service';
 import { Web3Service } from '../services/web3.service';
@@ -13,25 +14,31 @@ import { ColorPipe } from '../pipes/color.pipe';
   styleUrls: ['./block-map.component.scss']
 })
 export class BlockMapComponent implements OnInit {
-  blocks: BlockInfo[][] = [];
+  blocks: BlockInfo[] = [];
+  blocksRowAmount: Array<number> = [];
+  blocksColAmount: Array<number> = [];
+  selectedBlock: BlockInfo | null = null;
+  currentUserAddress: string | null = null;
+  isLoading: Observable<boolean>;
 
   constructor(
     private readonly blockService: BlockService,
     private readonly web3Service: Web3Service
-  ) {}
+  ) {
+    this.isLoading = this.blockService.isLoading$;
+  }
 
   async ngOnInit() {
     this.blockService.blocks$.subscribe((blocks) => {
       this.blocks = blocks;
+      this.blocksRowAmount = this.blocksColAmount = Array(this.blockService.rowAmount);
     });
-  }
-
-  checkIsSelected(block: any) {
-    return this.blockService.getSelectedBlock()?.id === block.id;
-  }
-
-  checkIsOwned(block: any) {
-    return block.owner === this.web3Service.getCurrentAddress();
+    this.web3Service.userAddressObservable$.subscribe((address) => {
+      this.currentUserAddress = address;
+    });
+    this.blockService.selectedBlock$.subscribe((block) => {
+      this.selectedBlock = block;
+    });
   }
 
   onBlockClick(block: any) {
--- src/app/services/block.service.ts
+++ src/app/services/block.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { EventEmitterSingleton } from '@js-emitter/event-emitter-light';
-import { BehaviorSubject, firstValueFrom } from 'rxjs';
+import { BehaviorSubject, forkJoin } from 'rxjs';
 import { BlockInfo, ContractService } from './contract.service';
 import { AuthService } from './auth.service';
 
@@ -8,11 +8,14 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class BlockService {
-  private rowAmount = 100;
-  private blocksSubject = new BehaviorSubject<BlockInfo[][]>([]);
-  blocks$ = this.blocksSubject.asObservable();
+  public rowAmount = 100;
+  public allSize = this.rowAmount * this.rowAmount;
+  private blocksSubject = new BehaviorSubject<BlockInfo[]>([]);
+  public blocks$ = this.blocksSubject.asObservable();
   private selectedBlockSubject = new BehaviorSubject<BlockInfo | null>(null);
-  selectedBlock$ = this.selectedBlockSubject.asObservable();
+  public selectedBlock$ = this.selectedBlockSubject.asObservable();
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  public isLoading$ = this.isLoadingSubject.asObservable();
 
   constructor(
     private readonly contractService: ContractService,
@@ -41,34 +44,38 @@ export class BlockService {
     return this.selectedBlockSubject.getValue();
   }
 
-  private setBlocks(blocks: BlockInfo[][]) {
+  private setBlocks(blocks: BlockInfo[]) {
     this.blocksSubject.next(blocks);
   }
 
   public replaceBlock(block: BlockInfo) {
     const blocks = this.blocksSubject.getValue();
-    const row = Math.floor(Number(block.id) / this.rowAmount);
-    const col = Number(block.id) % this.rowAmount;
-    blocks[row][col] = block;
+    blocks[Number(block.id)] = block;
     this.setBlocks(blocks);
   }
 
   async loadBlocks() {
-    const allSize = this.rowAmount * this.rowAmount;
-    for (let i = 0; i < allSize; i += this.rowAmount) {
-      const blocksInfo: BlockInfo[] = await firstValueFrom(this.contractService.getAllBlocksInfo(i, i + this.rowAmount - 1));
-      this.setBlocks([
-        ...this.blocksSubject.getValue(),
-        ...this.chunkArray(blocksInfo, this.rowAmount),
-      ]);
+    this.isLoadingSubject.next(true);
+    const batchSize = 500; // batch size
+    const requests = [];
+  
+    // Create allSize / batchSize requests
+    for (let i = 0; i < this.allSize; i += batchSize) {
+      const endIndex = Math.min(i + batchSize - 1, this.allSize - 1);
+      requests.push(this.contractService.getAllBlocksInfo(i, endIndex));
     }
-  }
-
-  private chunkArray(array: BlockInfo[], size: number): BlockInfo[][] {
-    const chunked = [];
-    for (let i = 0; i < array.length; i += size) {
-      chunked.push(array.slice(i, i + size));
-    }
-    return chunked;
+  
+    // Send all requests in parallel
+    forkJoin(requests).subscribe({
+      next: (results: BlockInfo[][]) => {
+        const allBlocks = results.flat();
+        this.setBlocks(allBlocks);
+        this.isLoadingSubject.next(false);
+      },
+      error: (error) => {
+        console.error('Error loading blocks:', error);
+        this.isLoadingSubject.next(false);
+      }
+    });
   }
 }
