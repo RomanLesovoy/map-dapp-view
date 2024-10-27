67db5a48c45fabb2445401b69515cc2dfd93c199
RomanLesovoy <romadesire97@gmail.com>
2024-10-27 12:26:34 +0200

    firebase + prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  contractAddress: '0x9539BA6983061954c86E848b2bf575f36A78950e',
+  apiUrl: 'https://map-dapp.onrender.com/api',
+  contractAddress: '0xf84845A6e3290fFacb1866cc80a0DFf6E813d2A0',
 };
